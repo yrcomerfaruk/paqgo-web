4b9aaf8f67968c8acfb8f07b7995a2bd4aeb1433
yrcomerfaruk <omerfarukyericer@gmail.com>
2026-09-10 03:10:06 +0300

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['192.168.1.107', 'localhost:3000'],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
